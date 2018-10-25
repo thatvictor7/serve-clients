4a0fb0076d2016c16286657a212059c7d1a31b02
thatvictor7 <montoya.victor7@gmail.com>
2018-10-25 11:09:49 -0600

    some changes

--- index.js
+++ index.js
@@ -18,7 +18,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-let port = 3000
+let port = process.env.PORT || 3000
 
 
 // Route imports
@@ -60,4 +60,4 @@ function errorHandler(err, req, res, next) {
 }
 
 
-app.listen(port, () => console.log('Server running on port 3000'))
+app.listen(port, () => console.log(`Server running on port ${port}`))
