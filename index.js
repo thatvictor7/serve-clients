c981ba836b1f135e1c82f1c892fa2719f5fedb38
thatvictor7 <montoya.victor7@gmail.com>
2018-10-25 10:55:47 -0600

    changes for heroku

--- index.js
+++ index.js
@@ -1,16 +1,63 @@
+// const express = require('express')
+// const app = express()
+// const port = 3000
+// const cors = require('cors')
+// const bodyParser = require('body-parser')
+//
+// app.use(cors())
+// app.use(bodyParser.urlencoded({ extended: false}))
+//
+// app.get('/',(req,res,next) => {
+//   res.send('🦖🔥')
+// })
+//
+// app.listen(port, () => {
+//   console.log(`i got you on ${port}`);
+// })
+
 const express = require('express')
-const app = express()
-const port = 3000
-const cors = require('cors')
 const bodyParser = require('body-parser')
+const app = express()
+let port = 3000
 
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false}))
 
-app.get('/',(req,res,next) => {
-  res.send('🦖🔥')
+// Route imports
+const charactersRoutes = require('./routes/characters')
+
+
+// General middleware
+  // Applied to all requests
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+// Basic GET route that responds with emoji or something
+  // Lets us know the server is working
+app.get('/', (req, res) => {
+  res.send('😈😈😈😈😈')
 })
 
-app.listen(port, () => {
-  console.log(`i got you on ${port}`);
-})
+// Any requests that START with /characters, send to this router file
+app.use('/characters', charactersRoutes)
+
+
+// Error handlers as final use case if routes don't match or if errors are generated
+  // 404
+app.use(notFound)
+  // General purpose 'catch' all errors
+app.use(errorHandler)
+
+
+function notFound(req, res, next) {
+  res.status(404).send({ error: 'Not found!', status: 404, url: req.originalUrl })
+}
+
+// eslint-disable-next-line
+function errorHandler(err, req, res, next) {
+  console.error('ERROR', err)
+  const stack = process.env.NODE_ENV !== 'production' ? err.stack : undefined
+  res.status(500).send({ error: err.message, stack, url: req.originalUrl })
+}
+
+
+app.listen(port, () => console.log('Server running on port 3000'))
